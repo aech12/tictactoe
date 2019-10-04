3a6bbbf92cdf133fd22c36439051dc42bd1ac238
Alex Howez <aech-12@hotmail.com>
2019-10-03 21:37:38 -0300

    Updates

---
+++ precache-manifest.99ab75717d4d6893f4b2dbea37d41360.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/tictactoe/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "6b4aa7f6be80bcf68c7c",
+    "url": "/tictactoe/static/css/main.0d782fd2.chunk.css"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/tictactoe/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "7afa20d7f3b518b6762e",
+    "url": "/tictactoe/static/js/2.19460945.chunk.js"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/tictactoe/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/tictactoe/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/tictactoe/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/tictactoe/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/tictactoe/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/tictactoe/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/tictactoe/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/tictactoe/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/tictactoe/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/tictactoe/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "6b4aa7f6be80bcf68c7c",
+    "url": "/tictactoe/static/js/main.1713d69b.chunk.js"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/tictactoe/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/tictactoe/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/tictactoe/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/tictactoe/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/tictactoe/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/tictactoe/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/tictactoe/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/tictactoe/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/tictactoe/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/tictactoe/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/tictactoe/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/tictactoe/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "7afa20d7f3b518b6762e",
+    "url": "/tictactoe/static/css/2.0ead4ac2.chunk.css"
+  },
+  {
+    "revision": "762752cc9adf3e71f0ea641730929b4f",
+    "url": "/tictactoe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tictactoe/precache-manifest.3d6b0d4098bba0f9f2b10c25a9313fbe.js"
+  "/tictactoe/precache-manifest.99ab75717d4d6893f4b2dbea37d41360.js"
 );
 
 workbox.clientsClaim();
